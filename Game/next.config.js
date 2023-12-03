c8f7f66efcfa343406de75204fc9b0069248c4ec
macdo <ing.macdo@gmail.com>
2023-12-03 19:47:28 -0300

    .

--- Game/next.config.js
+++ Game/next.config.js
@@ -4,10 +4,10 @@ const nextConfig = {
     styledComponents: true,
   },
   env: {
-    // URL_PADRAO: "https://funcombat.online/api/",
-    // URL_PADRAO_SOCKET: "wss://funcombat.online/api",
-    URL_PADRAO: "http://localhost:8000/api/",
-    URL_PADRAO_SOCKET: "ws://localhost:8000/api",
+    URL_PADRAO: "https://funcombat.online/api/",
+    URL_PADRAO_SOCKET: "wss://funcombat.online/api",
+    // URL_PADRAO: "http://localhost:8000/api/",
+    // URL_PADRAO_SOCKET: "ws://localhost:8000/api",
   },
   reactStrictMode: false,
 };
