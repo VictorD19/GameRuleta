2c7c73c020a4ebb0e5f0378f3439498a3fefa145
macdo <ing.macdo@gmail.com>
2023-12-06 21:32:02 -0300

    .

--- Server/reloadServer.sh
+++ Server/reloadServer.sh
@@ -1,4 +1,5 @@
 git pull
+docker builder prune
 docker image prune -f
 docker image rm -f gameruleta-game_app nginx gameruleta-fastapi_app
 docker container rm -f gameruleta-game_app-1 gameruleta-nginx_proxy-1 gameruleta-fastapi_app-1
