c2d298c0bd8bb159e61fbe54d6975fa86e64a9c5
VictorD <bertramvictor61@gmail.com>
2023-12-09 20:35:02 -0300

    Ajuste

--- Game/src/app/layout.js
+++ Game/src/app/layout.js
@@ -16,15 +16,13 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      <script type="text/javascript">
+      <head><script type="text/javascript">
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "k3g8ehl35v");
-</script>
-</head>
+</script></head>
       <body
         className={`${inter.className} row mx-0`}
         style={{ background: paleta.NEGRO, color: paleta.BLANCO }}
