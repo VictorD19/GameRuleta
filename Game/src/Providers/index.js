cdfb8e0b87af4d97c7a4c7877acdb7a4bae486b3
VictorD <bertramvictor61@gmail.com>
2023-12-13 00:40:25 -0300

    Ajuste

--- Game/src/Providers/index.js
+++ Game/src/Providers/index.js
@@ -4,16 +4,17 @@ import { useEffect } from "react";
 import { clarity } from "react-microsoft-clarity";
 import facebookPixel from "react-facebook-pixel";
 import { ID_MICROSOFT_CLARITY, PIXEL_FACEBOOK_ID } from "@/Api";
-import { useRouter } from "next/router";
+import { useSelectedLayoutSegment } from "next/navigation";
+
 export const Providers = ({ children }) => {
-  const router = useRouter();
   useEffect(() => {
     clarity.init(ID_MICROSOFT_CLARITY);
     facebookPixel.init(PIXEL_FACEBOOK_ID);
     facebookPixel.pageView();
-    router.events.on("routeChangeComplete", () => {
-      facebookPixel.pageView();
-    });
   }, []);
+  const activeSegment = useSelectedLayoutSegment();
+  useEffect(() => {
+    facebookPixel.pageView();
+  }, [activeSegment]);
   return <ContextAppProvider>{children}</ContextAppProvider>;
 };
