16b3b09ff1e6bc4f8d5913fda05bafbe372c9bb4
Mitter Duarte <59698584+mitdua@users.noreply.github.com>
2023-12-13 11:42:26 -0300

    ajuste ChatRoutes.py
    ...

--- Server/app/Routes/ChatRoutes.py
+++ Server/app/Routes/ChatRoutes.py
@@ -1,3 +1,4 @@
+import json
 import asyncio
 from fastapi import APIRouter, WebSocket
 
@@ -10,8 +11,8 @@ mensagems = []
 async def websocket_chat_general(websocket: WebSocket):
     await websocket.accept()    
     while True:        
-        mensaje = await websocket.receive_json()
-        mensagems.insert(0, mensaje)
+        mensaje = await websocket.receive_json()        
+        mensagems.insert(0, json.loads(mensaje))
         mensagems = mensagems[0:50]
         await websocket.send_json(mensagems)
         await asyncio.sleep(1)
