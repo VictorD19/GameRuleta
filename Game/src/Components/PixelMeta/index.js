01c52fe0891fa7472d8a3de2201dfcdf9cc07dbb
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2023-12-13 08:18:37 -0300

    Ajuste pixel

--- Game/src/Components/PixelMeta/index.js
+++ Game/src/Components/PixelMeta/index.js
@@ -1,18 +1,14 @@
 "use client";
-import facebookPixel from "react-facebook-pixel";
 import { useEffect } from "react";
 import { PIXEL_FACEBOOK_ID } from "@/Api";
 
 export const PixelMeta = () => {
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      import("react-facebook-pixel")
-        .then((x) => x.default)
-        .then((reactpixel) => {
-          reactpixel.init(PIXEL_FACEBOOK_ID);
-          reactpixel.pageView();
-        });
-    }
+    (async () => {
+      const facebookPixel = await import("react-facebook-pixel");
+      facebookPixel.default.init(PIXEL_FACEBOOK_ID);
+      facebookPixel.default.pageView();
+    })();
   });
   return null;
 };
--- Game/src/app/layout.js
+++ Game/src/app/layout.js
@@ -7,6 +7,7 @@ const inter = Archivo({ subsets: ["latin"] });
 import { Providers } from "@/Providers";
 import { Footer } from "@/Components/Footer";
 import "./globals.css";
+import { PixelMeta } from "@/Components/PixelMeta";
 
 export const metadata = {
   title: "FunCombat",
@@ -17,36 +18,27 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script
+        {/* <script
+          key="fb_pixel"
           dangerouslySetInnerHTML={{
-            __html: `
-            <!-- Meta Pixel Code -->
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '902299578160614');
-            fbq('track', 'PageView');
-            <!-- End Meta Pixel Code -->
-              `,
+            __html: `!function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', '902299578160614');
+                  fbq('track', 'PageView');`,
           }}
-        />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=902299578160614&ev=PageView&noscript=1"
-/>`,
-          }}
-        ></noscript>
+        /> */}
       </head>
       <body
         className={`${inter.className} row mx-0`}
         style={{ background: paleta.NEGRO, color: paleta.BLANCO }}
       >
+        <PixelMeta/>
         {/* Provider   */}
         <Providers>
           <div className="col-12 col-md-2">
