5c18f284e360f0f7a47b66315200272104909b74
macdo <ing.macdo@gmail.com>
2023-12-14 07:22:30 -0300

    ajuste chat general

--- Server/app/Routes/ChatRoutes.py
+++ Server/app/Routes/ChatRoutes.py
@@ -9,10 +9,20 @@ mensagems = []
 
 @router.websocket("/chat-general")
 async def websocket_chat_general(websocket: WebSocket):
-    await websocket.accept()    
-    while True:        
-        mensaje = await websocket.receive_json()        
-        mensagems.insert(0, json.loads(mensaje))
-        mensagems = mensagems[0:50]
+    
+    global mensagems
+
+    await websocket.accept()  
+
+    while True:       
+         
+        try:
+            mensaje = await asyncio.wait_for(websocket.receive_text(), timeout=1.0)
+        except asyncio.TimeoutError:
+            mensaje = None
+
+        if mensaje:       
+            mensagems.insert(0, json.loads(mensaje))
+            mensagems = mensagems[0:50]
         await websocket.send_json(mensagems)
         await asyncio.sleep(1)
